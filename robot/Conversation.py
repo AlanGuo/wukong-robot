0439150a1faa079f9baa9b61f0e77307ce9e521b
AlanGuo <AlanGuo>
2024-08-29 12:37:01 +0800

    grid

--- robot/Conversation.py
+++ robot/Conversation.py
@@ -165,7 +165,7 @@ class Conversation(object):
                 self.player.stop()
             else:
                 # 没命中技能，使用机器人回复
-                if self.ai.STREAM:
+                if hasattr(self.ai, "STREAM") and self.ai.STREAM:
                     stream = self.ai.stream_chat(query)
                     self.stream_say(stream, True, onCompleted=self.checkRestore)
                 else:
